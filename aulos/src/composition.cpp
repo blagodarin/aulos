0a6d826dc8b375457217120748dcf9336680d02b
Sergey Blagodarin <s.blagodarin@gmail.com>
2019-10-27 13:34:36 +0300

    Support specifying tracks in multiple parts

--- aulos/src/composition.cpp
+++ aulos/src/composition.cpp
@@ -24,9 +24,9 @@ namespace aulos
 {
 	CompositionImpl::CompositionImpl(const char* source)
 	{
-		bool startTrack = true;
+		size_t trackIndex = 0;
 
-		const auto parseNote = [&](const char* data, size_t baseOffset) {
+		const auto parseNote = [&](std::vector<NoteInfo>& track, const char* data, size_t baseOffset) {
 			assert(baseOffset >= 0 && baseOffset < 12);
 			if (*data == '#')
 			{
@@ -44,13 +44,59 @@ namespace aulos
 			}
 			if (*data < '0' || *data > '9')
 				throw std::runtime_error{ "Bad composition" };
-			if (startTrack)
-			{
-				_tracks.emplace_back();
-				startTrack = false;
-			}
-			_tracks.back().emplace_back(static_cast<Note>((*data - '0') * 12 + baseOffset));
+			track.emplace_back(static_cast<Note>((*data - '0') * 12 + baseOffset));
 			return data + 1;
+		};
+
+		const auto parseNotes = [&](std::vector<NoteInfo>& track) {
+			for (;;)
+			{
+				switch (*source++)
+				{
+				case '\0':
+					return false;
+				case '\r':
+					if (*source == '\n')
+						++source;
+					[[fallthrough]];
+				case '\n':
+					return true;
+				case '\t':
+				case ' ':
+					while (*source == ' ' || *source == '\t')
+						++source;
+					break;
+				case '.':
+					if (track.empty())
+						track.emplace_back(Note::Silence);
+					else
+						++track.back()._duration;
+					break;
+				case 'A':
+					source = parseNote(track, source, 9);
+					break;
+				case 'B':
+					source = parseNote(track, source, 11);
+					break;
+				case 'C':
+					source = parseNote(track, source, 0);
+					break;
+				case 'D':
+					source = parseNote(track, source, 2);
+					break;
+				case 'E':
+					source = parseNote(track, source, 4);
+					break;
+				case 'F':
+					source = parseNote(track, source, 5);
+					break;
+				case 'G':
+					source = parseNote(track, source, 7);
+					break;
+				default:
+					throw std::runtime_error{ "Bad composition" };
+				}
+			}
 		};
 
 		for (;;)
@@ -59,50 +105,27 @@ namespace aulos
 			{
 			case '\0':
 				return;
-			case '\n':
-				startTrack = true;
-				break;
 			case '\r':
 				if (*source == '\n')
 					++source;
-				startTrack = true;
+				[[fallthrough]];
+			case '\n':
+				trackIndex = 0;
 				break;
 			case '\t':
 			case ' ':
-				while (*source == '\t' || *source == ' ')
+				while (*source == ' ' || *source == '\t')
 					++source;
 				break;
-			case '.':
-				if (startTrack)
-				{
+			case ';':
+				while (*source && *source != '\n' && *source != '\r')
+					++source;
+				continue;
+			case '|':
+				if (trackIndex == _tracks.size())
 					_tracks.emplace_back();
-					startTrack = false;
-				}
-				if (_tracks.back().empty())
-					_tracks.back().emplace_back(Note::Silence);
-				else
-					++_tracks.back().back()._duration;
-				break;
-			case 'A':
-				source = parseNote(source, 9);
-				break;
-			case 'B':
-				source = parseNote(source, 11);
-				break;
-			case 'C':
-				source = parseNote(source, 0);
-				break;
-			case 'D':
-				source = parseNote(source, 2);
-				break;
-			case 'E':
-				source = parseNote(source, 4);
-				break;
-			case 'F':
-				source = parseNote(source, 5);
-				break;
-			case 'G':
-				source = parseNote(source, 7);
+				if (!parseNotes(_tracks[trackIndex++]))
+					return;
 				break;
 			default:
 				throw std::runtime_error{ "Bad composition" };
