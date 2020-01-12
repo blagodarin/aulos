691bbc20c18c675a5d5f6f904f60ee70663e7d96
Sergey Blagodarin <s.blagodarin@gmail.com>
2020-01-12 18:33:52 +0300

    Simplify fragment graphics

--- studio/src/fragment_item.cpp
+++ studio/src/fragment_item.cpp
@@ -30,17 +30,16 @@ namespace
 	struct FragmentColors
 	{
 		QColor _background;
-		QColor _handle;
 		QColor _outline;
 	};
 
 	const std::array<FragmentColors, 6> kFragmentColors{
-		FragmentColors{ "#f88", "#f00", "#800" },
-		FragmentColors{ "#ff8", "#ff0", "#880" },
-		FragmentColors{ "#8f8", "#0f0", "#080" },
-		FragmentColors{ "#8ff", "#0ff", "#088" },
-		FragmentColors{ "#88f", "#00f", "#008" },
-		FragmentColors{ "#f8f", "#f0f", "#808" },
+		FragmentColors{ "#f88", "#800" },
+		FragmentColors{ "#ff8", "#880" },
+		FragmentColors{ "#8f8", "#080" },
+		FragmentColors{ "#8ff", "#088" },
+		FragmentColors{ "#88f", "#008" },
+		FragmentColors{ "#f8f", "#808" },
 	};
 }
 
@@ -57,26 +56,23 @@ FragmentItem::FragmentItem(size_t trackIndex, size_t offset, size_t sequenceInde
 
 void FragmentItem::paint(QPainter* painter, const QStyleOptionGraphicsItem*, QWidget*)
 {
-	const auto bodyLeft = _rect.left() + kScaleX / 2;
-	const auto bodyRight = _rect.right() - kScaleX / 2;
 	painter->setPen(kFragmentColors[_colorIndex]._outline);
-	painter->setBrush(kFragmentColors[_colorIndex]._handle);
-	painter->drawRect({ _rect.topLeft(), QPointF{ bodyLeft, _rect.bottom() } });
-	painter->setBrush(kFragmentColors[_colorIndex]._background);
-	const std::array<QPointF, 5> bodyAndTail{
-		QPointF{ bodyLeft, _rect.top() },
-		QPointF{ bodyRight, _rect.top() },
+	const auto right = _rect.right() - kScaleX / 2;
+	const std::array<QPointF, 5> shape{
+		_rect.topLeft(),
+		QPointF{ right, _rect.top() },
 		QPointF{ _rect.right(), _rect.center().y() },
-		QPointF{ bodyRight, _rect.bottom() },
-		QPointF{ bodyLeft, _rect.bottom() },
+		QPointF{ right, _rect.bottom() },
+		_rect.bottomLeft(),
 	};
-	painter->drawConvexPolygon(bodyAndTail.data(), static_cast<int>(bodyAndTail.size()));
+	painter->setBrush(kFragmentColors[_colorIndex]._background);
+	painter->drawConvexPolygon(shape.data(), static_cast<int>(shape.size()));
 	if (_length > 1)
 	{
 		auto font = painter->font();
 		font.setPixelSize(kScaleY / 2);
 		painter->setFont(font);
-		painter->drawText(QRectF{ QPointF{ bodyLeft + kScaleX / 2, _rect.top() }, QPointF{ bodyRight, _rect.bottom() } }, Qt::AlignVCenter, QString::number(_sequenceIndex + 1));
+		painter->drawText(QRectF{ QPointF{ _rect.left() + kScaleX / 2, _rect.top() }, QPointF{ right, _rect.bottom() } }, Qt::AlignVCenter, QString::number(_sequenceIndex + 1));
 	}
 }
 
