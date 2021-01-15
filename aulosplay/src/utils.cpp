2f10d86531f8e16f9bb62dc3c2bca1ca73dd27c4
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-01-15 23:35:49 +0300

    Implement SSE acceleration of mono-to-stereo conversion

--- aulosplay/src/utils.cpp
+++ aulosplay/src/utils.cpp
@@ -4,14 +4,36 @@
 
 #include "utils.hpp"
 
+#if defined(_M_AMD64) || defined(_M_IX86) || defined(__amd64) || defined(__i386)
+#	ifdef _MSC_VER
+#		include <intrin.h>
+#	else
+#		include <x86intrin.h>
+#	endif
+#	define HAS_SSE 1
+#else
+#	define HAS_SSE 0
+#endif
+
 namespace aulosplay
 {
 	void monoToStereo(float* output, const float* input, size_t frames) noexcept
 	{
-		for (const auto end = output + 2 * frames; output < end; ++input)
+#if HAS_SSE
+		for (; frames > 4; frames -= 4)
+		{
+			const auto mono = _mm_load_ps(input);
+			_mm_store_ps(output, _mm_shuffle_ps(mono, mono, 0b01010000));
+			_mm_store_ps(output + 4, _mm_shuffle_ps(mono, mono, 0b11111010));
+			input += 4;
+			output += 8;
+		}
+#endif
+		for (; frames > 0; --frames)
 		{
 			*output++ = *input;
 			*output++ = *input;
+			++input;
 		}
 	}
 }
