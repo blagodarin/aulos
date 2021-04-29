c74b1629cdc7bd9a8794325362d91e2c088b0205
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-04-30 00:30:19 +0300

    Add test source file for every aulos header without a source file

---
+++ tests/aulos/src/tables.cpp
@@ -0,0 +1,7 @@
+// This file is part of the Aulos toolkit.
+// Copyright (C) Sergei Blagodarin.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <aulos/src/tables.hpp>
+
+static_assert(aulos::kNoteFrequencies[aulos::Note::A4] == 440);
